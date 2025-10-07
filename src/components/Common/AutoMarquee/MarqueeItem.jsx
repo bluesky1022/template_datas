455ac0d0c4fcc897c7e0e1e2d249059b6a5d6f0d
bluesky1022 <rts@1022.com>
2025-10-07 15:30:50 +0200

    helol

--- src/components/Common/AutoMarquee/MarqueeItem.jsx
+++
@@ -1,9 +0,0 @@
-export const MarqueeItem = (props) => {
-  const { label } = props;
-  return (
-    <div className="text-primary flex items-center mx-[20px] flex-nowrap">
-      <img src="/assets/images/icon-blue.webp" height={20} width={20} />
-      <span className="ml-[5px] text-nowrap">{label}</span>
-    </div>
-  );
-};
--- src/components/Common/Container.jsx
+++
@@ -1,9 +0,0 @@
-export const Container = (props) => {
-  const { children, className = "" } = props;
-
-  return (
-    <div className={"w-full px-12 xl:px-24 " + className}>
-      <div className="w-full max-w-[1320px] mx-auto">{children}</div>
-    </div>
-  );
-};
